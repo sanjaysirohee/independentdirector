7c4d4fbe51e903eaf2c9d2c1e8f439c4732b1e8c
raginiveloxn17 <ragini.k@veloxn.com>
2026-04-15 18:17:54 +0530

    admin logo corrected
    done

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -3,26 +3,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Logo: React.FC = () => {
-
   return (
     <Link href="/">
       <Image
-        src={getImgPath("/images/logo/logo.png")}
+        src="/images/logo/logo.png"
         alt="logo"
-        width={160}
-        height={50}
+        width={10}
+        height={10}
         style={{ width: 'auto', height: 'auto' }}
         quality={100}
-        className='dark:hidden'
+        className="dark:hidden"
       />
       <Image
-        src={getImgPath("/images/logo/logo-white.svg")}
+        src="/images/logo/logo.png"
         alt="logo"
         width={160}
         height={50}
         style={{ width: 'auto', height: 'auto' }}
         quality={100}
-        className='dark:block hidden'
+        className="dark:block hidden"
       />
     </Link>
   );
